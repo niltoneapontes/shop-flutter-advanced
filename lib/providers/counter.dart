5b7b9e435d6465cba98b47bc6252a54711f5e9f4
niltoneapontes <niltoneapontes@gmail.com>
2021-10-18 22:15:44 -0300

    feat: add first provider

---
+++ lib/providers/counter.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+
+class CounterState {
+  int _value = 0;
+
+  void inc() => _value++;
+  void dec() => _value--;
+  int get value => _value;
+}
+
+class CounterProvider extends InheritedWidget {
+  final CounterState state = CounterState();
+
+  CounterProvider({required Widget child}) : super(child: child);
+
+  static CounterProvider? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<CounterProvider>();
+  }
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+    return true;
+  }
+}
